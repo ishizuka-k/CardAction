acc9b675fb6a0d498537eaffcb2bdac19231dc3f
石塚昂 <syanisun@gmail.com>
2014-10-23 07:10:41 +0900

    no message

---
+++ CardAction/Assets/CardAction/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+    GameObject Player;
+    GameObject LookAt;
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player");
+        LookAt = GameObject.Find("Area8");
+        this.gameObject.transform.Translate(new Vector3(Player.transform.position.x, Player.transform.position.y + 10, Player.transform.position.z+3));
+        this.gameObject.transform.LookAt(LookAt.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ CardAction/Assets/CardAction/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    static 
+    GameObject[] Area = new GameObject[18];
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0 ; i < 18 ; i++)
+        {
+            string Aname = "Area" + (i+1);
+            Debug.Log(Aname);
+            Area[i] = GameObject.Find(Aname);
+            if (i < 9)
+            {
+                Area[i].renderer.material.color = Color.blue;
+            }
+            else
+            {
+                Area[i].renderer.material.color = Color.red;
+            }
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetMouseButtonDown(0)) {
+ 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+ 
+        if (Physics.Raycast(ray, out hit)){
+            GameObject obj = hit.collider.gameObject;
+            Debug.Log(obj.name);
+        }
+    }
+	}
+}
