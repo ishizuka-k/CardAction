e317f9eb5c310e9320cdd02169f94c5164dca0d4
石塚昂 <syanisun@gmail.com>
2014-10-27 00:22:52 +0900

    unitychanChange

--- CardAction/Assets/CardAction/Resources/Script/GameManager.cs
+++ CardAction/Assets/CardAction/Resources/Script/GameManager.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
     GameObject[] Area = new GameObject[18];
+    Player pPlayer;
+    //GameObject pPlayer;
 	// Use this for initialization
 	void Start () {
+        //pPlayer = GameObject.Find("Player");
+        pPlayer = GetComponent<Player>();
         for(int i = 0 ; i < 18 ; i++)
         {
             string Aname = "Area" + (i+1);
@@ -12,27 +16,32 @@ public class GameManager : MonoBehaviour {
             if (i < 9)
             {
                 Area[i].renderer.material.color = Color.blue;
+                Area[i].tag = "OwnArea";
             }
             else
             {
                 Area[i].renderer.material.color = Color.red;
+                Area[i].tag = "EnemyArea";
             }
         }
-	
+        Debug.Log(pPlayer);
 	}
 	
 	// Update is called once per frame
 	void Update () {
     if (Input.GetMouseButtonDown(0)) {
 
-			Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+		Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit = new RaycastHit();
  
         if (Physics.Raycast(ray, out hit)){
             GameObject obj = hit.collider.gameObject;
 
-			if (obj.tag == "Area"){
-				}
+            if (obj.tag == "OwnArea")
+            {
+                pPlayer.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y + obj.renderer.bounds.size.y, obj.transform.position.z);
+                //GetComponent<Player>().animator.SetInteger("State", 1);
+			}
             Debug.Log(obj.name);
         }
     }
---
+++ CardAction/Assets/CardAction/Resources/Script/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour {
+    public Animator animator;
+	// Use this for initialization
+	void Start () {
+        //アニメーター初期化
+        animator = GetComponent<Animator>();
+
+        //初期値設定
+        Vector3 StartPos = GameObject.Find("Area2").transform.position;
+        this.gameObject.transform.position = new Vector3(StartPos.x, StartPos.y + GameObject.Find("Area2").renderer.bounds.size.y, StartPos.z);
+        Debug.Log("X" + this.gameObject.transform.position.x + " Y " + this.gameObject.transform.position.y + " Z " + this.gameObject.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //animator.SetInteger("State", 2);
+        //Debug.Log("X");
+	}
+}
