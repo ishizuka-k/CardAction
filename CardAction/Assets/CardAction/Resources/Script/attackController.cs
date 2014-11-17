75066c1633404265fa87094715c82fec3d102e00
ishizuka-k <ishizuka-k@klab.com>
2014-11-17 16:14:09 +0900

    ワイド、ロング追加、エフェクトマネジャー追加

--- CardAction/Assets/CardAction/Resources/Script/attackController.cs
+++ CardAction/Assets/CardAction/Resources/Script/attackController.cs
@@ -39,13 +39,17 @@ public class attckContoller {
 						case CardData.TYPE.SORD:
 								switch (CD.id) {
 								case 0:
-										pPlayer.rigor = CD.rigor;
-										sordRange ();
-										break;
+									pPlayer.rigor = CD.rigor;
+									sordRange ();
+									break;
 								case 1:
-										break;
+									pPlayer.rigor = CD.rigor;
+									longSordRange();
+									break;
 								case 2:
-										break;
+									pPlayer.rigor = CD.rigor;
+									wideSordRange();
+									break;
 								}
 								break;
 						case CardData.TYPE.BULLET:
@@ -76,16 +80,78 @@ public class attckContoller {
         Object.Instantiate(effect, pos, Quaternion.identity);
 
 		foreach(GameObject obj in objs) {
-			enemy pEnemy = obj.GetComponent<enemy>();
-
-			if ( Range == pEnemy.AreaIdx ) {
-				pEnemy.nHp -= 1;
-				if ( pEnemy.nHp <= 0 ) {
-					Object.Destroy(obj);
-				}
-
+			if( EnemyHit(Range,obj) ) {
 				return true;
 			}
+		}
+		return false;
+	}
+
+	public bool wideSordRange () {
+		pPlayer.animator.SetInteger("State", 4);
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("Enemy");
+		int[] Range = {pPlayer.AreaIdx + 3,pPlayer.AreaIdx + 4,pPlayer.AreaIdx + 2};
+
+		//エフェクト
+		GameObject effect = (GameObject)Resources.Load<GameObject>("prefab/skillAttack");
+
+		if (Range [0] <= 18) {
+			foreach (GameObject obj in objs) {
+					EnemyHit (Range [0], obj);
+					GameObject Area = GameObject.Find ("Area" + Range [0]);
+					Vector3 pos = new Vector3 (Area.transform.position.x, Area.transform.position.y + 1.0f, Area.transform.position.z);
+					Object.Instantiate (effect, pos, Quaternion.identity);
+					if ((pPlayer.AreaIdx - 1) % 3 != 2) {
+							EnemyHit (Range [1], obj);
+							Area = GameObject.Find ("Area" + Range [1]);
+							pos = new Vector3 (Area.transform.position.x, Area.transform.position.y + 1.0f, Area.transform.position.z);
+							Object.Instantiate (effect, pos, Quaternion.identity);
+					}
+					if ((pPlayer.AreaIdx - 1) % 3 != 0) {
+							EnemyHit (Range [2], obj);
+							Area = GameObject.Find ("Area" + Range [2]);
+							pos = new Vector3 (Area.transform.position.x, Area.transform.position.y + 1.0f, Area.transform.position.z);
+							Object.Instantiate (effect, pos, Quaternion.identity);
+					}
+			}
+		}
+		return false;
+	}
+
+	public bool longSordRange () {
+		pPlayer.animator.SetInteger("State", 4);
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("Enemy");
+		int[] Range = {pPlayer.AreaIdx + 3,pPlayer.AreaIdx + 6};
+		
+		//エフェクト
+		GameObject effect = (GameObject)Resources.Load<GameObject>("prefab/skillAttack");
+
+		if( Range[0] <= 18 ) {
+			foreach(GameObject obj in objs) {
+				EnemyHit(Range[0],obj);
+				GameObject Area = GameObject.Find("Area" + Range[0]);
+				Vector3 pos = new Vector3(Area.transform.position.x,Area.transform.position.y+1.0f,Area.transform.position.z);
+				Object.Instantiate(effect, pos, Quaternion.identity);
+
+				if( Range[1] <= 18 ) {
+				EnemyHit(Range[1],obj);
+				Area = GameObject.Find("Area" + Range[1]);
+				pos = new Vector3(Area.transform.position.x,Area.transform.position.y+1.0f,Area.transform.position.z);
+				Object.Instantiate(effect, pos, Quaternion.identity);
+				}
+			}
+		}
+		return false;
+	}
+
+	private bool EnemyHit (int nRange,GameObject obj) {
+		enemy pEnemy = obj.GetComponent<enemy>();
+		if ( nRange == pEnemy.AreaIdx ) {
+			pEnemy.nHp -= 1;
+			if ( pEnemy.nHp <= 0 ) {
+				Object.Destroy(obj);
+			}
+			return true;
 		}
 		return false;
 	}
---
+++ CardAction/Assets/CardAction/Resources/Script/effectManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+public class effData {
+	public GameObject effect;
+	public string path;
+}
+public class effectManager : MonoBehaviour {
+	effData[] effectData;
+	int m_idx;
+
+	// Use this for initialization
+	void Start () {
+		m_idx = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int setEffect( string effectPath ) {
+
+		for (int i = 0; i < m_idx; i++) {
+			if (effectData[m_idx].path == effectPath) {
+					return i;
+			}
+		}
+		//エフェクト
+		effectData[m_idx].effect = (GameObject)Resources.Load<GameObject>(effectPath);
+		return m_idx++;
+	}
+
+	public bool startEffect(int idx,Vector3 pos) {
+		Object.Instantiate(effectData[idx].effect, pos, Quaternion.identity);
+		return true;
+		}
+}
